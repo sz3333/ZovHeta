c0b8aaace9935d979c672df5261e42f030694716
Artem <foxyy437@gmail.com>
2025-02-21 14:27:12 +0200

    Update FHeta.py

--- FHeta.py
+++ FHeta.py
@@ -10,7 +10,7 @@ __version__ = (9, 0, 6)
 
 #meta banner: https://raw.githubusercontent.com/Fixyres/FHeta/refs/heads/main/IMG_20241127_111104_471.jpg
 # meta pic: https://raw.githubusercontent.com/Fixyres/FHeta/refs/heads/main/IMG_20241127_111101_663.jpg
-# ©️ Fixyresl, 2024
+# ©️ Fixyres, 2024
 # 🌐 https://github.com/Fixyres/FHeta
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
