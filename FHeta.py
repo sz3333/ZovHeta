490948fff50a9b0f03215344717bd6faa4fa030b
Fixyres <foxyy437@gmail.com>
2025-08-27 01:07:08 +0300

    Update FHeta.py

--- FHeta.py
+++ FHeta.py
@@ -23,7 +23,7 @@ try:
     import certifi
     assert certifi.__version__ == "2024.8.30"
 except (ImportError, AssertionError):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "certifi==2024.8.30", "--break-system-packages"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "certifi==2024.8.30"])
 
 @loader.tds
 class FHeta(loader.Module):
